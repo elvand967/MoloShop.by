b930e236b1efd42b8cc559aa62ca9ceb76373c07
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2024-11-04 16:09:58 +0300

    Adding a blog app

---
+++ app_packages/blog/apps.py
@@ -0,0 +1,8 @@
+# D:\Python\myProject\MoloShop\app_packages\blog\apps.py
+
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
